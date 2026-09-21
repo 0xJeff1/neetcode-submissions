694785491d3d4ef32a98fd20031ea43db7c88ab7
0x_jeff <78095454+0xJeff1@users.noreply.github.com>
2026-09-21 14:41:16 +0100

    Add: binary-search - submission-13

---
+++ Data Structures & Algorithms/binary-search/submission-13.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int helper_function(std::vector<int>& nums , int target , int start , int end)
+{
+    int mid = (start + end) / 2;
+
+    if(start > end)
+    {
+        return -1;
+    }
+
+    if(nums[mid] == target)
+    {
+        return mid;
+    }
+    
+    if(nums[mid] < target)
+    {
+        return helper_function(nums,target,mid + 1,end);
+    }
+    else
+    {
+        return helper_function(nums,target,start,mid - 1);
+    }
+
+}
+    int search(vector<int>& nums, int target) 
+    {
+         return helper_function(nums,target,0,nums.size() - 1);    
+    }
+};
